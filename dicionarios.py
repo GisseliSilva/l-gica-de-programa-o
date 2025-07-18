4d29c216fc081b4f80ee6d2d62d14590c0f57219
Gisseli Rocha Silva <gisseli.rocha@icloud.com>
2025-07-18 13:00:48 -0300

    Add files via upload

---
+++ dicionarios.py
@@ -0,0 +1,12 @@
+nome_dicionario = '''{chave1: valor1,
+                   chave2: valor2,
+                   chave3: valor3}'''
+
+D = {'arroz': 30.00, 'feijao': 9.00, 'alcatra':45.00,'alface':3.00}
+print(D)
+
+print(D['alcatra'])
+
+
+
+for chave, valor 
